16dc2dbf24f791be12e47ab5cf9fe63ca8f86823
carlos <carlos.molina.munoz@gmail.com>
2018-04-27 20:44:03 -0300

    primera carpeta termianda

--- 1 metodos/ejercicio4.rb
+++ 1 metodos/ejercicio4.rb
@@ -1,3 +1,14 @@
 # Crear un método que muestre en pantalla un saludo, el método
 # debe recibir un parámetro, si ese parámetro es el string "Hola" el
 # método debe mostrar en pantalla "Hola Mundo".
+
+def saludo (m)
+  if m == "Hola"
+    puts "Hola Mundo"
+  else
+    puts "Hola a todos"
+  end
+end
+
+puts "Escriba un mensaje:"
+saludo(gets.chomp)
--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,35 @@
 # Crear un método que reciba como parámetro dos números enteros positivos
 # e imprima los números pares que existen entre esos dos números.
+
+def pares (a,b)
+  min = 0
+  max = 0
+  if a < b
+    min = a
+    max = b
+  elsif a > b
+    min = b
+    max = a
+  else
+    if a.even?
+      puts a
+    else
+      puts "Sólo hay impares"
+    end
+    return
+  end
+  muestra_pares(min, max)
+end
+
+def muestra_pares(a, b)
+  (a..b).each do |i|
+    puts i if i.even?
+  end
+end
+
+puts "Ingrese el primer número:"
+a = gets.chomp.to_i
+puts "Ingrese el segundo número:"
+b = gets.chomp.to_i
+puts " "
+pares(a, b)
